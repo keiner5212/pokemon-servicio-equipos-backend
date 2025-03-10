589fda0d5ea9648cf9fe8305b85c04fdd94b0541
Keiner Alvarado <kjgameplay0000@gmail.com>
2025-03-10 16:46:59 -0500

    fix: docker crlf compatibility fixes

---
+++ docker/firebase-local/entrypoint.js
@@ -0,0 +1,28 @@
+const { execSync } = require("child_process");
+const fs = require("fs");
+
+const FIREBASE_PROJECT = process.env.FIREBASE_PROJECT || "default-project";
+const main = () => {
+
+  if (!fs.existsSync("firebase.json")) {
+    console.log("Firebase data not found. Skipping Firebase Emulator start...");
+    return
+  }
+
+  execSync(
+    `firebase emulators:start --only auth,firestore,storage,database --project ${FIREBASE_PROJECT}--export-on-exit`,
+    { stdio: "inherit" }
+  );
+}
+try {
+  main()
+} catch (error) {
+  console.log(error.message);
+  setTimeout(() => {
+    try {
+      main()
+    } catch (error) {
+      console.log(error.message);
+    }
+  }, 5000);
+}
